d7106216eacf4d87dfd293978aacef09dd71ef83
evgeny <khokhlov.e.n@gmail.com>
2025-03-10 11:30:21 +0000

    chore: use `callbackFlow` builder for Flow extensions

--- chat-android/src/main/java/com/ably/chat/FlowUtils.kt
+++ chat-android/src/main/java/com/ably/chat/FlowUtils.kt
@@ -1,22 +1,15 @@
 package com.ably.chat
 
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.callbackFlow
 
 /**
  * Converts a callback-based API to a Flow with an unlimited buffer to handle backpressure
  */
-internal inline fun <T> transformCallbackAsFlow(crossinline subscribe: ((T) -> Unit) -> Subscription): Flow<T> = flow {
-    val channel = Channel<T>(Channel.UNLIMITED)
-
-    val subscription = subscribe { channel.trySend(it) }
-
-    try {
-        for (item in channel) {
-            emit(item)
-        }
-    } finally {
-        subscription.unsubscribe()
-    }
-}
+internal inline fun <T> transformCallbackAsFlow(crossinline subscribe: ((T) -> Unit) -> Subscription): Flow<T> = callbackFlow {
+    val subscription = subscribe { trySend(it) }
+    awaitClose { subscription.unsubscribe() }
+}.buffer(Channel.UNLIMITED)
