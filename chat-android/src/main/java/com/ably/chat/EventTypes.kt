4d5d82e2d3a117c87cea0e7454f6ed8ceee2ac26
sacOO7 <sachinshinde7676@gmail.com>
2025-04-01 17:51:22 +0530

    [ECO-5265] Added RoomEvents enum to EventTypes

--- chat-android/src/main/java/com/ably/chat/EventTypes.kt
+++ chat-android/src/main/java/com/ably/chat/EventTypes.kt
@@ -98,3 +98,15 @@ public enum class RoomReactionEventType(public val eventName: String) {
      */
     Reaction("roomReaction"),
 }
+
+/**
+ * Room events.
+ */
+public enum class RoomEvents(public val event: String) {
+    /**
+     * Event triggered when a discontinuity is detected in the room's channel connection.
+     * A discontinuity occurs when an attached or update event comes from the channel with resume=false,
+     * except for the first attach or attaches after explicit detach calls.
+     */
+    Discontinuity("room.discontinuity")
+}
